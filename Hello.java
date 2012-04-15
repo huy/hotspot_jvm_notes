f5e435070b57b79c4aa8930da566aff4d3b5ab8a
Le Huy <lehuy20@gmail.com>
2012-04-15 18:28:28 +0200

    rename

--- Hello.java
+++ Hello.java
@@ -1,9 +1,15 @@
+import java.util.Random;
+
 public class Hello{
   public static void main(String[] arg){
      System.out.println("Say Hello");
+
+     Random r = new Random();
      int sum =1;
-     for(int i=0; i < 1000*100; i++)
-        sum = sum + i;
+     for(int i=0; i < 1000; i++){
+       int[] a = new int[5*1024*1024]; // hope to kickoff GC
+       sum += a[r.nextInt(5*1024*1024-1)];
+     }
 
      try {
        Thread.sleep(1000*60);   
