8376b78bd0b4d26e5b7fbad9fd3c9aaf6962a5c8
Le Huy <lehuy20@gmail.com>
2012-05-05 22:11:39 +0200

    add -XX:+PrintCompilation to observe hotspot compilation

--- samples/test_volatile.sh
+++ samples/test_volatile.sh
@@ -2,5 +2,6 @@ export LD_LIBRARY_PATH=`pwd`
 java  -XX:+UnlockDiagnosticVMOptions \
 -XX:+PrintAssembly \
 -XX:PrintAssemblyOptions="hsdis-print-bytes" \
+-XX:+PrintCompilation \
 -server -cp . TestVolatile
 
